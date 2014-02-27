3bf46fe59a8bdf4cce0ef632a596628127464bc8
Baptiste Bertrand-Rapello <baptiste@BBR-PC-HP.(none)>
2014-02-27 16:13:54 +0100

    modif exception.cpp et calculator.cpp

--- Exception.cpp
+++ Exception.cpp
@@ -5,7 +5,46 @@
 // Login   <koszyc_l@epitech.net>
 // 
 // Started on  Thu Feb 27 02:14:23 2014 Koszyczek Laurent
-// Last update Thu Feb 27 02:14:25 2014 Koszyczek Laurent
+// Last update Thu Feb 27 16:10:42 2014 Bertrand-Rapello Baptiste
 //
 
 #include "Exception.hh"
+
+Exception::Exception(const std::string & msg)
+{
+  this->msg = msg;
+}
+
+Exception::~Exception()
+{
+
+}
+
+SyntaxErrorException::SyntaxErrorException(const std::string & msg)
+{
+  this->msg = "SyntaxErrorException: " + msg;
+}
+
+SyntaxErrorException::~SyntaxErrorException()
+{
+}
+
+BadParameterException::BadParameterException(const std::string & msg)
+{
+  this->msg = "BadParameterException: " + msg;
+}
+
+BadParameterException::~BadParameterException()
+{
+
+}
+
+DivByZeroException::DivByZeroException(const std::string & msg)
+{
+  this->msg = "DivByZeroException: " + msg;
+}
+
+DivByZeroException::~DivByZeroException()
+{
+
+}
